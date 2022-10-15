6ca44fe17c1855a5c66bab97d025bb490179af2a
rltzvetkov <115454733+rltzvetkov@users.noreply.github.com>
2022-10-15 05:49:05 +0300

    Delete folder

--- BasicSyntax/Rounding.js
+++
@@ -1,8 +0,0 @@
-function rounding(number, precision) {
-    if(precision >=15){
-        precision = 15;
-    }
-    let result = number.toFixed(precision)
-    console.log(parseFloat(result));
-}
-rounding(3.14867545342457696675554644533534334, 16);
